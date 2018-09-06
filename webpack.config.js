41842a1cd39e5e90c4304d083def8b81a5a98655
David Nascimento <davidpn.11@gmail.com>
2018-09-05 22:43:58 -0300

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,11 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
 })
 module.exports = {
   entry: path.join(__dirname, 'examples/src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'async-style.js',
+    libraryTarget: 'commonjs2',
+  },
   module: {
     rules: [
       {
